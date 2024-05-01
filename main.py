a1243339e7cf77796365835afe2d8f37c14a066e
Rishi10898 <sriramr@wsc.school.nz>
2024-05-02 10:32:46 +1200

    VERSION 1

---
+++ main.py
@@ -0,0 +1,43 @@
+# Greetings
+Print ("Hello, Welcome to Our Quiz V .1 ")
+input ("")
+print("May I know your name?")
+input("")
+print ("that's good")
+#Response 1
+Print ("I will ask you a few questions and you should be able to answer those questions")
+input("")
+#First Question
+print("What is the capital of Australia?")
+input("")
+Answer_1 = Canberra 
+if Answer_1:
+    print("Correct")
+if Answer_1 != canberra
+print ("Worng Answer")
+print ("The correct answer is Canberra!")
+input("")
+print("that's fine, now we go to the next question")    
+input("")
+# Second Question
+print ("What's the capital of New Zealand and what's the national Language of New Zealand")
+input("")
+Anwer_2 = Wellington and Maori/English/NZ Sign Language
+if Anwer_2:
+    print("Correct")
+if Anwer_2 !=  Wellington and Maori/English/NZ Sign Language
+print ("Wrong Answer")
+print ("The correct answer is  Wellington and Maori/English/NZ Sign Language")
+input("")
+print("That's fine we will proceed with the next one!")
+# Third Question
+print ("What is the National Animal of India ")
+input("")
+Answer_3 = Royal Bengal Tiger
+if Answer_3:
+    print("Correct")
+if Answer_3 != Royal Bengal Tiger
+print ("Wrong Answer")
+print ("Thanks for Playing the Questions are over now!")
+print ("How much have you got right?")
+input()
