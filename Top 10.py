2d7cc007c0f5fa1c62050d200d8d7656da34e08f
Rishi10898 <168684167+Rishi10898@users.noreply.github.com>
2024-06-04 14:25:24 +1200

    wsc

--- Top 10.py
+++ Top 10.py
@@ -3,7 +3,7 @@
 def intro():
     name = input("What's your name?")
     print("Welcome to this quiz,", name)
-    print("Do you know the 10 largesst countries in the world?, if so can you name them?")
+    print("Do you know the 10 largest countries in the world?, if so can you name them?")
 def get_Passes():
     passes_user = input("How many chances do you want?")
     try: 
@@ -16,8 +16,20 @@ def get_Passes():
         print("Invalid nummber of passes")
 Largest_Countries_in_the_World = ["Russia", "Canada", "China", "United States", "Brazil", "Australia", "India", "Argentina", "Kazakhstan", "Algeria"]
 score = 0
+def Check_in_list(answer, list):
+    if answer in list:
+        return True
+    else:
+        return False
 while get_Passes() > 0:
     answer = input("Name one of the top 10 biggest countries in the world").lower()
+    if Check_in_list(answer, Largest_Countries_in_the_World):
+        print("Correct Answer")
+        score += 5
+    else:
+        print("Wrong Answer")
+        passes_user -= 
+
 #------MAIN FUNCTIONS------ 
 intro()
 get_Passes()
