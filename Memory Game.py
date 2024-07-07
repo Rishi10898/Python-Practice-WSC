1ff834d76fac2eaace5e4f27b06ec21e7d44e9fd
Rishi10898 <168684167+Rishi10898@users.noreply.github.com>
2024-07-07 01:31:09 +0000

    wsc

--- Memory Game.py
+++ Memory Game.py
@@ -19,5 +19,32 @@ def understand_rule():
 def ask_user_topic():
     print("There are 3 topics based on which the game is made.")
     print("You have to choose any one of those 3 topics.")
-def memory_game_start():
-    input("")
+
+def memory_game_sart():
+    User_topic = print ("There are 3 topics they are :- World countries and its capitals(combined), Indian states and its capitals + Districts of Tamil nadu(Combined), States of USA and its capitals(Combined)")
+    print("World/India/USA :- These are the topics you have to choose")
+    User_topic = input("Which topic based on which you want to play now?")
+    if User_topic == "World" or " World" or "India" or " India" or "USA" or " USA":
+        print()
+# USA states and its capitals
+Starts_with_A = ["Alabama","Alaska", "Arizona", "Arkansas", "Atlanta", "Augusta","Annapolis", "Austin", "Albany" ]
+Starts_with_B = ["Boise", "Baton Rouge","Boston","Bismarck"]
+Starts_with_C = ["California","Colorado","Connecticut ","Carson City","Concord","Columbia", "Charleston", "Cheyenne", "Columbus"]
+Starts_with_D = ["Delaware ", "Denver", "Dover", "Des Moines"]
+Starts_with_F = ["Florida ", "Frankfort"]
+Starts_with_G = "Georgia"
+Starts_with_H = ["Hawaii ", "Hartford", "Honolulu", "Helena", "Harrisburg"]
+Starts_with_I = ["Idaho","Illinois","Indiana" ,"Iowa", "Indianapolis"]
+Starts_with_J = ["Juneau", "Jackson","Jefferson City" ]
+Starts_with_K = ["Kansas", "Kentucky"] 
+Starts_with_L = ["Louisiana", "Little Rock","Lansing","Lincoln"]
+Starts_with_M = ["Maine","Maryland","Massachusetts","Michigan","Minnesota","Mississippi","Missouri","Montana", "Montpelier", "Madison", "Montgomery"] 
+Starts_with_N = ["Nebraska","Nevada","New Hampshire","New Jersey","New Mexico","New York","North Carolina","North Dakota", "Nashville"]
+Starts_with_O = ["Ohio","Oklahoma","Oregon", "Oklahoma City", "Olympia"]
+Starts_with_P = ["Pennsylvania", "Phoenix","Providence","Pierre"]
+Starts_with_R = ["Rhode Island", "Raleigh", "Richmond"]
+Starts_with_S = ["South Carolina","South Dakota","Sacramento","Springfield","Saint Paul", "Santa Fe","Salem","Salt Lake City"]
+Starts_wiht_T = ["Tennessee","Texas","Tallahassee","Topeka","Trenton"] 
+Starts_with_U = "Utah" 
+Starts_with_V = ["Vermont","Virginia"]
+Starts_with_W = ["Washington","West Virginia","Wisconsin","Wyoming"]
